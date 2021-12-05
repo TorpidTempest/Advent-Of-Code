2232601d23b5300cb3c20480ac91e7caba1da118
TorpidTempest <lis4dyslexia@gmail.com>
2021-12-05 11:38:37 +0000

    git unignored inputs

--- get_input.py
+++ get_input.py
@@ -2,4 +2,5 @@ def get_input(text_file):
     input_file = open(text_file, "r")
     input_data = input_file.read()
     input_list = input_data.splitlines()
+    input_file.close()
     return input_list
