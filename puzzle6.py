f4de80fcd56d2d893bdad823f0c5921bfed2c0a6
TorpidTempest <lis4dyslexia@gmail.com>
2021-12-27 20:38:01 +0000

    Puzzle 6, stuck but not sure why

---
+++ puzzle6.py
@@ -0,0 +1,46 @@
+# %%
+from get_input import get_input
+
+raw_input = get_input("input_data/puzzle6.txt")
+input_list = []
+for line in raw_input:
+    input_list = line.split(',')
+int_list = []
+for number in input_list:
+    int_list.append(int(number))
+
+def a_day_passes(a_list, cycle_length, maturity_lag):
+    next_gen = 0
+    output = []
+    for fish in a_list:
+        if fish == 0:
+            output.append(cycle_length - 1)
+            next_gen += 1
+        else:
+            output.append(fish - 1)
+    for _ in range(next_gen):
+        output.append(cycle_length + maturity_lag - -1)
+    return output
+
+
+def puzzle1(a_list):
+    days = 80
+    cycle_length = 7
+    maturity_lag = 2
+    for i in range(days + 1):
+        a_list = a_day_passes(a_list, cycle_length, maturity_lag)
+        print(f'There are {len(a_list)} lanternfish after {i} days')
+            
+# for _ in range(80)
+#     next_gen = 0
+#     for fish in int_list:
+#         if fish == 0:
+#             fish = cycle_length - 1
+#             next_gen += 1
+#         else:
+#             fish -= 1
+#     for _ in range(next_gen):
+#         int_list.append(cycle_length + maturity_lag - -1)
+#     return a_list
+
+# %%
