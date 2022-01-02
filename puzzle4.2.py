7ea594d6fed042d25f3e344d9745a6eef235faa4
TorpidTempest <lis4dyslexia@gmail.com>
2022-01-02 14:55:23 +0000

    Complete

--- puzzle4.2.py
+++ puzzle4.2.py
@@ -1,39 +1,42 @@
 # %%
 # Storing and cleaning the data for called list
-file_1 = open("input_data/puzzle4-1.txt", "r")
-call_data = file_1.read()
-call_list = call_data.split(',')
-file_1.close()
+def main():
 
-# Storing and cleaning data for bingo_boards, splitting string into boards
-file_2 = open("input_data/puzzle4-2.txt", "r")
-board_data = file_2.read()
-bingo_boards_messy = board_data.split('\n\n')
-file_2.close()
+    file_1 = open("input_data/puzzle4-1.txt", "r")
+    call_data = file_1.read()
+    call_list = call_data.split(',')
+    file_1.close()
 
-# Splitting boards into lines
+    # Storing and cleaning data for bingo_boards, splitting string into boards
+    file_2 = open("input_data/puzzle4-2.txt", "r")
+    board_data = file_2.read()
+    bingo_boards_messy = board_data.split('\n\n')
+    file_2.close()
 
-bingo_boards_interim = []
+    # Splitting boards into lines
 
-for board in bingo_boards_messy:
-    bingo_boards_interim.append(board.splitlines())
+    bingo_boards_interim = []
 
-# Splitting lines into elements
-bingo_boards = []
+    for board in bingo_boards_messy:
+        bingo_boards_interim.append(board.splitlines())
 
-for board in bingo_boards_interim:
-    new_board = []
-    for line in board:
-        new_board.append(line.split())
-    bingo_boards.append(new_board)
+    # Splitting lines into elements
+    bingo_boards = []
 
-final_boards = {}
+    for board in bingo_boards_interim:
+        new_board = []
+        for line in board:
+            new_board.append(line.split())
+        bingo_boards.append(new_board)
 
-for i in range(len(bingo_boards)):
-    final_boards.update({i+1 : bingo_boards[i]})
+    final_boards = {}
 
-called = {i + 1:[[False for _ in range(5)] for _ in range(5)] for i in range(len(bingo_boards))}
+    for i in range(len(bingo_boards)):
+        final_boards.update({i+1 : bingo_boards[i]})
 
+    called = {i + 1:[[False for _ in range(5)] for _ in range(5)] for i in range(len(bingo_boards))}
+    run(call_list, final_boards, called, True)
+    run(call_list, final_boards, called, False)
 """
 Sub functions from original solution to check for bingo on rows and columns 
 seperately. Perhaps not ideal but sufficient for now to test new
@@ -75,9 +78,9 @@ def run(call_list: list, boards: dict, called: dict, win: bool):
     final_number = None
     for number in call_list:
         for board in boards:
-            for i in range(rows):
-                for j in range(columns):
-                    if bingo_boards[board][i][j] == number:
+            for i in range(5):
+                for j in range(5):
+                    if boards[board][i][j] == number:
                         called[board][i][j] = True
         complete = check_all(called)
         if complete != []:
@@ -89,31 +92,34 @@ def run(call_list: list, boards: dict, called: dict, win: bool):
                     for j in range(columns):
                         if called[winning_board][i][j] == False:
                             remaining += int(boards[winning_board][i][j])
-                output = f"""The winning board is {complete[0]}
+                output = f"""
+                The winning board is {complete[0]}
                 The final number called was {final_number}
                 Sum of the remaining values is {remaining}
                 Answer = {final_number * remaining}
                 """
-                return output
+                return print(output)
+            elif len(boards) == 1:
+                final_number = int(number)
+                remaining = 0
+                key = list(boards.keys())[0]
+                for i in range(rows):
+                    for j in range(columns):
+                        if called[key][i][j] == False:
+                            remaining += int(boards[key][i][j])
+                output = f"""
+                The remaining board is {key}
+                The final number called was {final_number}
+                The sum of the remaining numbers on the board was {remaining}
+                The answer is {final_number * remaining}
+                """
+                return print(output)
             else:
                 for key in complete:
                     del boards[key], called[key]
-        
-        if len(boards) == 1:
-            final_number = int(number)
-            remaining = 0
-            key = list(boards.keys())[0]
-            for i in range(rows):
-                for j in range(columns):
-                    if called[key][i][j] == False:
-                        remaining += int(boards[key][i][j])
-            output = f"""The winning board is {key}
-            The final number called was {final_number}
-            The sum of the remaining numbers on the board was {remaining}
-            The answer is {final_number * remaining}
-            """
-            return output
-    
+
+if __name__ == '__main__':
+    main()
         
         
 # %%
