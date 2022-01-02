7a712caf1eb20eef43d895d360b5e26a7b4867fb
TorpidTempest <lis4dyslexia@gmail.com>
2022-01-02 20:24:13 +0000

    Puzzle part 1 updated, part 2 complete

--- puzzle6.py
+++ puzzle6.py
@@ -10,26 +10,47 @@ for number in input_list:
     int_list.append(int(number))
 
 def a_day_passes(a_list, cycle_length, maturity_lag):
-    next_gen = 0
-    output = []
-    for fish in a_list:
-        if fish == 0:
-            output.append(cycle_length - 1)
-            next_gen += 1
+    new_fish = 0
+    for i in range(len(a_list)):
+        if a_list[i] == 0:
+            a_list[i] = (cycle_length - 1)
+            new_fish += 1
         else:
-            output.append(fish - 1)
-    for _ in range(next_gen):
-        output.append(cycle_length + maturity_lag - -1)
-    return output
+            a_list[i] -= 1
+    for _ in range(new_fish):
+        a_list.append(cycle_length + maturity_lag - 1)
+    return a_list
 
 
 def puzzle1(a_list):
     days = 80
     cycle_length = 7
     maturity_lag = 2
-    for i in range(days + 1):
+    for i in range(1, days + 1):
         a_list = a_day_passes(a_list, cycle_length, maturity_lag)
-        print(f'There are {len(a_list)} lanternfish after {i} days')
+    print(f'There are {len(a_list)} lanternfish after {days} days')
             
+def puzzle2(a_list):
+    days = 256
+    cycle_length = 7
+    maturity_lag = 2
+    for i in range(1, 10 + 1):
+        a_list = a_day_passes(a_list, cycle_length, maturity_lag)
+    fishDict = {}
+    for fish in a_list:
+        if fish not in fishDict:
+            fishDict[fish] = 0
+        fishDict[fish] += 1
+    days_left = days - 10
+    for _ in range(days_left):
+        zeros = fishDict[0]
+        for day in range(8):
+            fishDict[day] = fishDict[day + 1]
+        fishDict[8] = zeros
+        fishDict[6] += zeros
+    fish_sum = 0
+    for day in fishDict:
+        fish_sum += fishDict[day]
+    print(f'After {days} days there are {fish_sum} fish')
 
 # %%
