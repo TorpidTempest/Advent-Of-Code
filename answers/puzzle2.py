584d8b5e7767a7cababf0a17ad28b7c9913274f2
Daniel Jones DOS <lis4dyslexia@gmail.com>
2022-08-17 15:45:32 +0100

    Updated packaging and added venv

---
+++ answers/puzzle2.py
@@ -0,0 +1,45 @@
+# %% Run me 1st if using VSCode Interactive Window
+from answers.get_input import get_input
+
+# %% Puzzle 2 Part 1
+input_list = get_input("input_data/puzzle2.txt")
+
+distance = 0
+depth = 0
+
+for command in input_list:
+    if 'forward' in command:
+        distance += int(''.join(filter(str.isdigit, command)))
+    if 'down' in command:
+        depth += int(''.join(filter(str.isdigit, command)))
+    if 'up' in command:
+        depth -= int(''.join(filter(str.isdigit, command)))
+
+print(distance)
+print(depth)
+print(distance*depth)
+
+# %% Puzzle 2 Part 2
+input_list = get_input("input_data/puzzle2.txt")
+
+distance = 0
+depth = 0
+aim = 0
+
+# I'm sure there is a more elegant way to pull ints from string but it works
+for command in input_list:
+    if 'forward' in command:
+        magnitude = int(''.join(filter(str.isdigit, command)))
+        distance += magnitude
+        depth += magnitude * aim
+    if 'down' in command:
+        aim += int(''.join(filter(str.isdigit, command)))
+    if 'up' in command:
+        aim -= int(''.join(filter(str.isdigit, command)))
+
+print(distance)
+print(depth)
+print(distance*depth)
+
+
+# %%
